27c5ebdd904fd3d0043ec1076d56aa0bf7d2b788
Maxime Wack <maximewack@free.Fr>
2016-05-17 00:12:30 +0200

    Function naming convention

--- R/build.R
+++ R/build.R
@@ -1,14 +1,14 @@
-stat_column <- function(stat, data)
+statColumn <- function(stat, data)
 {
   data %>%
     lapply(stat) %>%
     unlist
 }
 
-stat_table <- function(data, stats)
+statTable <- function(data, stats)
 {
   stats %>%
-    sapply(stat_column, data) -> tbl
+    sapply(statColumn, data) -> tbl
   stats %>%
     sapply(attr, "label") -> colnames(tbl)
 
