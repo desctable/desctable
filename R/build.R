30823d397a8f6b7992afbb3f43306449070dd88b
Maxime Wack <maximewack@free.Fr>
2016-05-17 00:13:19 +0200

    varColumn function to generate the variable names column (with factor levels) for the final table

--- R/build.R
+++ R/build.R
@@ -14,3 +14,26 @@ statTable <- function(data, stats)
 
   tbl
 }
+
+varColumn <- function(data, labels = NULL)
+{
+# Replace variable names by their labels, if they exist
+  names(data) -> base_names
+  base_names[base_names %in% names(labels)] <- labels[base_names[base_names %in% names(labels)]]
+
+  # Insert levels for factors after the variable name
+  if (any(data %>% lapply(is.factor) %>% unlist))
+  {
+    data %>%
+      lapply(is.factor) %>%
+      unlist %>%
+      which %>%
+      insert(x = base_names,
+             y = select(data, .) %>% lapply(levels),
+             position = .)
+  } else
+  {
+    base_names
+  }
+}
+
