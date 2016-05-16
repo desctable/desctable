de7003aadcdc5a13b4f64af980c25780390bc330
Maxime Wack <maximewack@free.Fr>
2016-05-17 00:47:46 +0200

    Documentation update for build.R functons

--- R/build.R
+++ R/build.R
@@ -1,3 +1,8 @@
+#' Generate one statistic for all variables
+#'
+#' @param stat The statistic to use
+#' @param data The dataframe to apply the statistic to
+#' @return A vector for one statistic column
 statColumn <- function(stat, data)
 {
   data %>%
@@ -5,6 +10,11 @@ statColumn <- function(stat, data)
     unlist
 }
 
+#' Generate the table of all statistics for all variables
+#'
+#' @param data The dataframe to apply the statistic to
+#' @param stat A list of statistics to use
+#' @return A dataframe of all statistics for all variables
 statTable <- function(data, stats)
 {
   stats %>%
@@ -15,6 +25,19 @@ statTable <- function(data, stats)
   tbl
 }
 
+#' Generate the variable column to display as row names
+#'
+#' Generates the variable column.
+#' Replaces the variable names by their label if given in the named character vector labels
+#' Inserts levels for factors
+#'
+#' labels is an option named character vector used to make the table prettier.
+#' If given, the variable names for which there is a label will be replaced by their corresponding label.
+#' Not all variables need to have a label, and labels for non-existing variables are ignored.
+#'
+#' @param data The dataframe to get the names from
+#' @param labels The optional named character vector containing the keypairs var = "Label"
+#' @return A character vector of variable names/labels/levels
 varColumn <- function(data, labels = NULL)
 {
 # Replace variable names by their labels, if they exist
