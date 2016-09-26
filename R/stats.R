07405abf81d20239c319cfa46d8dc3222b25f156
Maxime Wack <maximewack@free.Fr>
2016-09-27 00:38:04 +0200

    Brought back safe version of IQR

--- R/stats.R
+++ R/stats.R
@@ -101,3 +101,14 @@ Q3 <- function(x)
 {
   stats::quantile(x, .75, na.rm = T)
 }
+
+#' Return the inter-quartile range
+#'
+#' Safe version of IQR for statify
+#' @param x A vector
+#' @return The IQR
+#' @export
+IQR <- function(x)
+{
+  base::diff(stats::quantile(x, c(0.25, 0.75), na.rm = T))
+}
