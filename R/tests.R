2349d91c2f149bb24c41fe8821da54442b1e10b8
Maxime Wack <maximewack@free.Fr>
2017-04-21 01:32:00 +0200

    Formula version of chisq.test

--- R/tests.R
+++ R/tests.R
@@ -9,3 +9,15 @@ fisher.test.formula <- function(formula, ...)
 {
   fisher.test.default(x = eval(formula[[2]]), y = eval(formula[[3]]), ...)
 }
+
+chisq.test.default <- stats::chisq.test
+
+chisq.test <- function(...)
+{
+  UseMethod("chisq.test")
+}
+
+chisq.test.formula <- function(formula, ...)
+{
+  chisq.test.default(x = eval(formula[[2]]), y = eval(formula[[3]]), ...)
+}
