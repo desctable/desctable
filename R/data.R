8ddc1554515af5f56493fd79169434c1d385bc83
Maxime Wack <maximewack@free.Fr>
2017-04-28 11:34:59 +0200

    Deleted test dataset

--- R/data.R
+++
@@ -1,6 +0,0 @@
-#' Test data
-#'
-#' A dataset containing every type of variables encountered in statistical analyses,
-#' with NA and NaN values as well.
-#' @format A data frame with 13 rows and 7 variables
-"test_data"
